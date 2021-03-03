30e603a52d91c9800064f8fc3107a12e15cc1add
szirbikpeti <61188059+szirbikpeti@users.noreply.github.com>
2021-03-03 22:42:11 +0100

    Create setup.py

---
+++ fbchat/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='fbchat',
+      version='1.9.7',
+      description="""
+        Nothing
+      """,
+      url='http://theautomatic.net/yahoo_fin-documentation/',
+      author='Andrew Treadway',
+      author_email='opensourcecoder11@gmail.com',
+      license='MIT',
+      packages=['fbchat'],
+      keywords = ["client", "send",],
+zip_safe=False)
